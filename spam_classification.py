67e61bfa9af7e37e819b649dcd13f234ad153ea2
Alejandra Camelo Cruz <jaime.camelo.cruz@uni-potsdam.de>
2023-09-21 16:10:43 +0200

    improve code

--- spam_classification.py
+++ spam_classification.py
@@ -10,13 +10,13 @@ import scipy.io
 import pandas as pd
 import numpy as np
 import seaborn as sns
-from scipy.stats import randint
+from pprint import pprint
 from matplotlib import pyplot as plt
 
 # Model
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
-from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 
 def pie_plot(data, title):
@@ -24,87 +24,58 @@ def pie_plot(data, title):
     plt.figure(figsize=(8,8))
 
     plt.pie(
-        x=counts, 
+        x=counts,
         labels=['ham', 'spam'],
         autopct='%1.1f%%',
         explode=[0.05, 0.05],
         colors=sns.color_palette('Set2'))
-    
-    # Add Title 
+
     plt.title(
         label=title,
         pad=20)
-    
+
     plt.savefig(f'plots/{title}.png')
-    
-def visualize_features(X, Y, num_bins):
-    df = pd.DataFrame(X[Y == 1].sum(),  columns = ['spam'])
-    df['ham'] = X[Y == -1].sum()
-    
-    df1 = df.sample(frac =.005)
-    
-    axes = df1.plot.bar(rot=0, subplots=True)
-    axes[1].legend(loc=2)
-    
-    axes.set_ylim(0, 10000)
+    plt.show()
 
 
-# def main():
+def evaluation(y_test, y_pred, title):
+  accuracy = accuracy_score(y_test, y_pred)
+  matrix = confusion_matrix(y_test, y_pred)
+  matrix = matrix.astype('float') / matrix.sum(axis=1)[:, np.newaxis]
 
-# Load MATLAB file
-mat_data = scipy.io.loadmat('emails.mat')
+  plt.figure(figsize=(16,7))
+  sns.set(font_scale=1.4)
+  sns.heatmap(matrix, annot=True, annot_kws={'size':10},
+                cmap=plt.cm.Greens, linewidths=0.2)
+  class_names = ['ham', 'spam']
+  tick_marks = np.arange(len(class_names))
+  tick_marks2 = tick_marks + 0.5
+  plt.xticks(tick_marks, class_names, rotation=0)
+  plt.yticks(tick_marks2, class_names, rotation=0)
+  plt.xlabel('Predicted label')
+  plt.ylabel('True label')
+  plt.title(title)
+  plt.savefig(f'plots/{title}.png')
 
-# Extract data
-X = pd.DataFrame.sparse.from_spmatrix(mat_data['X'])
-X = X.T
-Y = mat_data['Y'][0]
-
-# How do data look like?
-missing_values = X.isnull().any()
-no_missing = missing_values[missing_values.isin([True])].empty
-print(f'There are no missing values: {no_missing}')
-
-# Shape of dataframe
-print(X.head())
-X.shape
-
-
-# Plot how data are distributed
-# pie_plot(Y, 'complete data (unbalanced)')
-
-# Data is mildly unbalanced but model can be tried
-#split train and test
-x_train, x_test, y_train, y_test = train_test_split(X, Y,
-                                                    random_state=4)
-
-#Try a first model
-clf = RandomForestClassifier(max_depth=2, random_state=4)
-clf.fit(x_train, y_train)
-
-y_pred = clf.predict(x_test)
-
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
+  print("Accuracy:", accuracy)
+  print(confusion_matrix(y_test, y_pred))
+  print(classification_report(y_test, y_pred))
+  plt.show()
 
 
-#tune hyperparameters
-param_dist = {'n_estimators': randint(50,500),
-              'max_depth': randint(1,20)}
+def parameter_test(x_train, x_test, y_train, y_test, max_depth=False):
+  random = 4
+  test = []
+  if max_depth:
+    title = 'max_depth'
+    for num in range(1,100):
+      clf = RandomForestClassifier(max_depth = num, random_state=random)
+      clf.fit(x_train, y_train)
+      y_pred = clf.predict(x_test)
+      accuracy = accuracy_score(y_test, y_pred)
+      test.append(accuracy)
 
-rand_search = RandomizedSearchCV(clf, 
-                                 param_distributions = param_dist, 
-                                 n_iter=5, 
-                                 cv=5)
+    plt.plot(test)
+    plt.savefig(f'plots/{title}.png')
+    plt.show()
 
-search = rand_search.fit(x_train, y_train)
-search.best_params_
-
-
-print('Best hyperparameters:',  search.best_params_)
-
-
-# pie_plot(y_train, 'train data (unbalanced)')
-# pie_plot(y_test, 'test data')
-
-
-# main()
